9af6eef720096a00df9cc06eca0e144c37642e72
romangojshik2@gmail.com <romangojshik2@gmail.com>
2025-12-01 20:05:14 +0300

    Initial Commit

---
+++ StateObject/StateObjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateObjectApp.swift
+//  StateObject
+//
+//  Created by Roman Gojshik on 1.12.25.
+//
+
+import SwiftUI
+
+@main
+struct StateObjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
